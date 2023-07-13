b450ce317e48f47a0d1b35d906605b1e52c29daf
Om Kathe <om.kathe@mindstix.com>
2023-07-13 16:13:15 +0530

    feat: catalan-number

--- Factorial.js
+++ Factorial.js
@@ -1,9 +1,9 @@
 function factorial(n) {
-  if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
 
 function displayFactorial() {
@@ -19,3 +19,19 @@ function displayFactorial() {
 }
 
 displayFactorial();
+
+function calculateCatalan(n) {
+    var catalan = []; // Array to store the Catalan numbers
+
+    catalan[0] = 1;
+    catalan[1] = 1;
+
+    for (var i = 2; i <= n; i++) {
+        catalan[i] = 0;
+        for (var j = 0; j < i; j++) {
+            catalan[i] += catalan[j] * catalan[i - j - 1];
+        }
+    }
+
+    return catalan[n];
+}
