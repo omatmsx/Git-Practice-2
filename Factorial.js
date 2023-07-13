c7b364c441c983695f7b61ac0bc479ff3205ed0a
Om Kathe <om.kathe@mindstix.com>
2023-07-13 16:09:58 +0530

    feat: add-fibonacci

--- Factorial.js
+++ Factorial.js
@@ -1,9 +1,9 @@
 function factorial(n) {
-  if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
 
 function displayFactorial() {
@@ -19,3 +19,18 @@ function displayFactorial() {
 }
 
 displayFactorial();
+
+function fibonacci(n) {
+    var sequence = [0, 1]; // Initialize the sequence with the first two numbers
+
+    if (n <= 2) {
+        return sequence.slice(0, n);
+    }
+
+    for (var i = 2; i < n; i++) {
+        var nextNumber = sequence[i - 1] + sequence[i - 2];
+        sequence.push(nextNumber);
+    }
+
+    return sequence;
+}
