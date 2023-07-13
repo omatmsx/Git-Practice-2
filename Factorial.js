a32972b964559aa9cbbce82be2c754f477a89c0b
Om Kathe <om.kathe@mindstix.com>
2023-07-13 13:05:59 +0530

    feat: display-factorial-fn

--- Factorial.js
+++ Factorial.js
@@ -5,3 +5,15 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }
+
+function displayFactorial() {
+    var number = parseInt(prompt("Enter a number:"));
+
+    if (isNaN(number)) {
+        console.log("Invalid input. Please enter a valid number.");
+        return;
+    }
+
+    var result = factorial(number);
+    console.log("The factorial of " + number + " is: " + result);
+}
